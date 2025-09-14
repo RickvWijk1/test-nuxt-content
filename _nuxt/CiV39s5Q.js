efcc4f8093f23dd15664c61fd187288d50170bbc
RickvWijk1 <58942595+RickvWijk1@users.noreply.github.com>
2025-09-14 14:06:33 +0000

    Deploying to gh-pages from @ RickvWijk1/test-nuxt-content@99506bc92a73745596f9d6c8f511379f291e8595 🚀

---
+++ _nuxt/CiV39s5Q.js
@@ -0,0 +1,1 @@
+import{r as t}from"./yYp8mfMO.js";import a from"./Cnlnk0qX.js";import{y as e,E as n,h as s,w as c,I as i}from"./DHdwrndB.js";import"./iik6CYzq.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
